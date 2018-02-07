ea07c870c4f10729dc07eb642d0918b7c19c8664
lzzkdxc <lzzkdxc@github.com>
2018-02-07 22:15:17 +0800

    q

--- 99_hua_rang_dao.cpp
+++ 99_hua_rang_dao.cpp
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include<windows.h>
 #include <conio.h>
+void hidecursor()
+{
+	CONSOLE_CURSOR_INFO cursor_info={1,0};//第二个值为零表示光标隐藏
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursor_info); 
+}
+/////////////////////////////////////////////////////////////////////////////////////
 	char a[9]={' ','1','2','3','4','5','6','7','8'},input;
 	int c1=0,c2=0,t,i,now,win=1;
 void change(char*a,char*b)
@@ -13,6 +19,10 @@ void change(char*a,char*b)
 }
 void start()
 {
+	hidecursor();//隐藏光标 
+	system("cls");
+	printf("Click any key to start."); 
+	input=getch();
 	srand(time(NULL));
 	for(i=1;i<=100;i++)
 	{
@@ -22,7 +32,6 @@ void start()
 		{
 			change(&a[c1],&a[c2]);
 		}
-		
 	}
 }
 void in()
@@ -36,18 +45,22 @@ void in()
 		change(&a[now],&a[now+1]);
 	if(input=='d'&&now!=0&&now!=3&&now!=6)
 		change(&a[now],&a[now-1]);
+	if(input=='b')
+		start();
 }
-void out()//输出 
+void out()
 {
 	system("cls");
 	for(i=0;i<=8;i++)
 	{
-		printf("%c",a[i]);
+		if(i%3==0)
+			puts("\n");
+		
+		printf("   %c",a[i]);
+		
 		if(a[i]==' ')
 			now=i;
-		if(i%3==2)
-			puts("");
-}
+	}
 	if(a[0]=='1'&&a[1]=='2'&&a[2]=='3'&&a[3]=='4'&&a[4]=='5'&&a[5]=='6'&&a[6]=='7'&&a[7]=='8'&&a[8]=='9')
 	{
 		win=0;
